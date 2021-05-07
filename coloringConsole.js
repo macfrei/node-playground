6164b089a2e2fc27485c8913f6476c9eaef38ae6
Marie-Carleen Frei <lene@7freiheiten.de>
2021-05-07 08:53:55 +0200

    Make CLI interaktive with readline

--- coloringConsole.js
+++ coloringConsole.js
@@ -6,3 +6,22 @@ const args = process.argv.slice(2);
 args.forEach((arg) => console.log(chalk.yellow(arg)));
 // Would be the same with nodes escape sequences
 args.forEach((arg) => console.log("\x1b[33m%s\x1b[0m", arg));
+
+// Making CLI (command line interface) interactive
+const readline = require("readline").createInterface({
+  // Taking an input from the terminal
+  input: process.stdin,
+  // Displays output --> our question in line 18
+  // If 'output: undefined' then the question won't be displayed
+  output: process.stdout,
+});
+
+// question method accepts query(output) and callback function
+// Callback function will be invoked once user inputs something
+readline.question("What's your name?", (name) => {
+  console.log(chalk.bgGreen(`Hello ${name}!`));
+  readline.close();
+});
+
+// If wanting to make a more complex CLI tool
+// Use inquirer library: 'npm install inquirer'
