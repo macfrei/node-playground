8cbc20c44ef36847a3f6b202da51d9a3e9ada74b
Marie-Carleen Frei <lene@7freiheiten.de>
2021-05-20 10:55:28 +0200

    Restructure files

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ This repo will be used to thoroughly learn node.js.
 ## Content
 
 - [notes](./notes.md)
+- [About Node.js](./aboutNode.md)
--- index.js
+++
@@ -1,5 +0,0 @@
-// Importing functionality from another file
-const getArgs = require("./exposeFunctionality");
-
-const args = getArgs();
-args.forEach((arg) => console.log(arg));
